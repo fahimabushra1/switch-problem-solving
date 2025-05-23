0acbf7fdd6887238c94bb12bcef8f37411884924
fatima arifeen <fatima.arifeen24@gmail.com>
2025-05-23 09:13:10 +0600

    fruit type

---
+++ switch-problem-solving/fruit-type.js
@@ -0,0 +1,14 @@
+// 4. Fruit Type
+let fruitType = "berry";
+switch (fruitType) {
+    case "apple":
+    case "mango":
+    case "banana":
+    case "berry":
+        console.log(`${fruitType} is yummyyyyyy`)
+        break;
+
+    default:
+        console.log("it's not fruit")
+        break;
+}
