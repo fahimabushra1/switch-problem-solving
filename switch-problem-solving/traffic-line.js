5a584efd2a40a59db0318534c650d37ece021903
fatima arifeen <fatima.arifeen24@gmail.com>
2025-05-23 08:53:51 +0600

    traffic line

---
+++ switch-problem-solving/traffic-line.js
@@ -0,0 +1,8 @@
+// 2. Traffic Light
+let trafficLight = "green" ;
+switch(trafficLight){
+    case "red" : console.log("stop"); break;
+    case "yellow" : console.log("wait"); break;
+    case "green" : console.log("go"); break;
+    default : console.log("no signal"); break;
+}
