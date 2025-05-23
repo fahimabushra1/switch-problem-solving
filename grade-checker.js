dd74c2f6926be27b992af35bc52814d72bd831c4
fatima arifeen <fatima.arifeen24@gmail.com>
2025-05-23 09:01:50 +0600

    graade checker

---
+++ grade-checker.js
@@ -0,0 +1,10 @@
+// 3. Grade Checker
+let gradePoint = "A" ;
+switch(gradePoint){
+    case "A" : console.log("excellent"); break;
+    case "B" : console.log("good"); break;
+    case "C" : console.log("fair"); break;
+    case "D" : console.log("poor"); break;
+    case "F" : console.log("fail"); break;
+    default : console.log("excellent"); break;
+}
